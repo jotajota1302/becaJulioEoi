2d720935fc3f5a81d05f429541a8d8b78a6a7fd4
jotajota1302 <jotajota1302@hotmail.com>
2024-08-28 18:44:13 +0200

    commit

--- springwebdemo/src/main/java/com/eoi/es/springwebdemo/PersonController.java
+++ springwebdemo/src/main/java/com/eoi/es/springwebdemo/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,77 +18,78 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/person")
 public class PersonController {
 
-	ArrayList<Person> personas=new ArrayList<Person>();
-	
+	ArrayList<Person> personas = new ArrayList<Person>();
+
 	{
-		personas.add(Person.builder().nombre("JJ").apellidos("JIMENEZ").build());	
-		personas.add(Person.builder().nombre("Maria").apellidos("RODRIGUEZ").build());	
+		personas.add(Person.builder().nombre("JJ").apellidos("JIMENEZ").build());
+		personas.add(Person.builder().nombre("Maria").apellidos("RODRIGUEZ").build());
 	}
-	
+
 	@GetMapping
 	@ResponseBody
-	public Person getPersona(@RequestParam(required = false) String nombre) {		
-		
+	public Person getPersona(@RequestParam(required = false) String nombre) {
+
 		for (Person person : personas) {
-			if(person.getNombre().equals(nombre)) {
+			if (person.getNombre().equals(nombre)) {
 				return person;
 			}
-		}				
+		}
 		return null;
 	}
-	
-	
+
 	@GetMapping(value = "/nombre/{nombre}")
 	@ResponseBody
-	public Person getPersonaByPath( @PathVariable String nombre) {		
-		
+	public Person getPersonaByPath(@PathVariable String nombre) {
+
 		for (Person person : personas) {
-			if(person.getNombre().equals(nombre)) {
+			if (person.getNombre().equals(nombre)) {
 				return person;
 			}
-		}				
+		}
 		return null;
 	}
-	
+
 	@GetMapping(value = "/apellidos/{apellidos}")
 	@ResponseBody
-	public Person getPersonaByApellidos( @PathVariable String apellidos) {		
-		
+	public Person getPersonaByApellidos(@PathVariable String apellidos) {
+
 		for (Person person : personas) {
-			if(person.getApellidos().equals(apellidos)) {
+			if (person.getApellidos().equals(apellidos)) {
 				return person;
 			}
-		}				
+		}
 		return null;
 	}
-	
+
 	@GetMapping(value = "/all")
 	@ResponseBody
-	public List<Person> getPersonas() {		
-		
+	public List<Person> getPersonas() {
+
 		return personas;
 	}
-	
+
 	@PostMapping
 	@ResponseBody
-	public void createPerson() {
+	public void createPerson(@RequestBody Person person) {
 
-		personas.add(Person.builder().nombre("JJ").apellidos("JIMENEZ").build());	
+		personas.add(person);
+
 	}
-	
+
 	@PutMapping
 	@ResponseBody
-	public void updatePerson() {
+	public void updatePersonSurname(@RequestBody Person person) {
+
+		personas.stream().filter(p -> p.getNombre().equals(person.getNombre())).findFirst().get()
+				.setApellidos(person.getApellidos());
 		
-		personas.get(personas.size()-1).setApellidos("RODRIGUEZ");
 	}
-	
+
 	@DeleteMapping
 	@ResponseBody
-	public void deletePerson() {
-		
-		personas.remove(personas.size()-1);
+	public void deletePerson(@RequestParam String nombre) {
+
+		personas.remove(personas.stream().filter(p -> p.getNombre().equals(nombre)).findFirst().get());
 	}
-	
-	
+
 }
