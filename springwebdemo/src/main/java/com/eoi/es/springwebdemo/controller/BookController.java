859ddab441f62fbb110bd846627148a1948b88f9
jotajota1302 <jotajota1302@hotmail.com>
2024-08-30 17:21:24 +0200

    commit

--- springwebdemo/src/main/java/com/eoi/es/springwebdemo/controller/BookController.java
+++ springwebdemo/src/main/java/com/eoi/es/springwebdemo/controller/BookController.java
@@ -1,8 +1,9 @@
 package com.eoi.es.springwebdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,35 +15,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eoi.es.springwebdemo.model.Book;
+import com.eoi.es.springwebdemo.entity.Book;
+import com.eoi.es.springwebdemo.repository.BookRepository;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
-	private List<Book> books = new ArrayList<>();
-
-	{
-		books.add(new Book(1, "Cien años de soledad", "Gabriel García Márquez", "Ficción"));
-		books.add(new Book(2, "1984", "George Orwell", "Distopía"));
-		books.add(new Book(3, "El señor de los anillos", "J.R.R. Tolkien", "Fantasía"));
-		books.add(new Book(4, "Don Quijote de la Mancha", "Miguel de Cervantes", "Clásico"));
-		books.add(new Book(5, "Harry Potter y la piedra filosofal", "J.K. Rowling", "Fantasía"));
-	}
+	@Autowired
+	private BookRepository repository;
 
 	@GetMapping
 	public ResponseEntity<List<Book>> findAll() {
 
-		return new ResponseEntity<List<Book>>(books, HttpStatus.OK);
+		return new ResponseEntity<List<Book>>(repository.findAll(), HttpStatus.OK);
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Book> findById(@PathVariable String id) {
+	public ResponseEntity<Book> findById(@PathVariable Integer id) {
 
-		for (Book book : books) {
-			if (book.getId() == Integer.valueOf(id)) {
-				return new ResponseEntity<Book>(book, HttpStatus.OK);
-			}
+		Optional<Book> resultado = repository.findById(id);
+
+		if (resultado.isPresent()) {
+			return new ResponseEntity<Book>(resultado.get(), HttpStatus.OK);
 		}
 
 		return new ResponseEntity<Book>(HttpStatus.NOT_FOUND);
@@ -52,53 +47,41 @@ public class BookController {
 	@PostMapping
 	public ResponseEntity<String> create(@RequestBody Book book) {
 
-		if (!books.contains(book)) {
-			books.add(book);
+		repository.save(book);
 
-			return new ResponseEntity<String>(HttpStatus.CREATED);
-		}
-		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<String>(HttpStatus.CREATED);
+
 	}
 
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<String> update(@RequestBody Book book, @PathVariable int id) {
 
 		if (id == book.getId()) {
-			for (Book tmp : books) {
-				if (tmp.getId() == Integer.valueOf(id)) {
-					tmp.setAuthor(book.getAuthor());
-					tmp.setCategory(book.getCategory());
-					tmp.setTitle(book.getTitle());
-				}
 
-				return new ResponseEntity<String>(HttpStatus.ACCEPTED);
-			}
+			repository.save(book);
+
+			return new ResponseEntity<String>(HttpStatus.ACCEPTED);
+
 		}
 
 		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 	}
 
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<String> deleteById(@PathVariable String id) {
+	public ResponseEntity<String> deleteById(@PathVariable Integer id) {
 
-		for (Book book : books) {
-			if (book.getId() == Integer.valueOf(id)) {
-				books.remove(book);
-				
-				return new ResponseEntity<String>(HttpStatus.ACCEPTED);
-			}
-		}
-		
-		return new ResponseEntity<String>(HttpStatus.BAD_REQUEST); 
+		repository.deleteById(id);
+
+		return new ResponseEntity<String>(HttpStatus.ACCEPTED);
 	}
 
 	@DeleteMapping
 	public ResponseEntity<String> deleteAll() {
 
-		books.removeAll(books);
+		repository.deleteAll();
 
 		return new ResponseEntity<String>(HttpStatus.ACCEPTED);
-		
+
 	}
 
 }
