f1b4ab946dfdf2e968369d7af0ab0394befa3bac
jotajota1302 <jotajota1302@hotmail.com>
2024-08-30 18:01:32 +0200

    commit

--- springwebdemo/src/main/java/com/eoi/es/springwebdemo/repository/BookRepository.java
+++ springwebdemo/src/main/java/com/eoi/es/springwebdemo/repository/BookRepository.java
@@ -1,10 +1,11 @@
 package com.eoi.es.springwebdemo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.eoi.es.springwebdemo.entity.Book;
 
-
+@Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
 	
 }
