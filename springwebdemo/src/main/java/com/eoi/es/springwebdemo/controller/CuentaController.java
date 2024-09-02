ff2e622381af8fdd41b85e02a6acddf4a50d5ee7
jotajota1302 <jotajota1302@hotmail.com>
2024-09-02 18:02:16 +0200

    remove imports

--- springwebdemo/src/main/java/com/eoi/es/springwebdemo/controller/CuentaController.java
+++ springwebdemo/src/main/java/com/eoi/es/springwebdemo/controller/CuentaController.java
@@ -1,18 +1,13 @@
 package com.eoi.es.springwebdemo.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
