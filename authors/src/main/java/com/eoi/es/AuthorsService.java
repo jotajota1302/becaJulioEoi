90f616c33fd9158de77cab3f4b72c712707c1098
jotajota1302 <jotajota1302@hotmail.com>
2024-09-03 18:36:46 +0200

    add format

--- authors/src/main/java/com/eoi/es/AuthorsService.java
+++ authors/src/main/java/com/eoi/es/AuthorsService.java
@@ -14,77 +14,78 @@ import org.springframework.web.client.RestTemplate;
 public class AuthorsService {
 
 	private static final String BOOKS_SERVICE_URL = "http://localhost:8082/books/author/";
-	
+
 	@Autowired
 	private AuthorsRepository authorsRepository;
-	
-	public List<AuthorDto> findAll(){
-		
-		List<AuthorDto> dtos=new ArrayList<AuthorDto>();		
-		List<Author> entities = authorsRepository.findAll();		
+
+	public List<AuthorDto> findAll() {
+
+		List<AuthorDto> dtos = new ArrayList<AuthorDto>();
+		List<Author> entities = authorsRepository.findAll();
 		for (Author author : entities) {
 			dtos.add(entityToDto(author));
-		}		
-		
-		return dtos;		
+		}
+
+		return dtos;
 	}
-	
-	public AuthorDto findById(Integer id){				
-		
+
+	public AuthorDto findById(Integer id) {
+
 		Author author = authorsRepository.findById(id).get();
-		
-		AuthorDto dto=entityToDto(author);
-		
-		RestTemplate restTemplate= new RestTemplate();
-		
-		ResponseEntity<List<BookDto>> booksResponse = restTemplate.exchange(BOOKS_SERVICE_URL.concat(author.getId()+""), HttpMethod.GET, null,
+
+		AuthorDto dto = entityToDto(author);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<List<BookDto>> booksResponse = restTemplate.exchange(
+				BOOKS_SERVICE_URL.concat(author.getId() + ""), HttpMethod.GET, null,
 				new ParameterizedTypeReference<List<BookDto>>() {
 				});
-		
+
 		dto.setBooks(booksResponse.getBody());
-		
+
 		return dto;
 	}
-	
+
 	public void create(AuthorDto dto) {
-		
-		Author entity = dtoToEntity(dto);		
+
+		Author entity = dtoToEntity(dto);
 		entity.setId(null);
-		
+
 		authorsRepository.save(entity);
-		
+
 	}
-	
+
 	public void update(AuthorDto dto) {
-		
-		Author entity = dtoToEntity(dto);		
-		
+
+		Author entity = dtoToEntity(dto);
+
 		authorsRepository.save(entity);
-		
+
 	}
-	
-	public void deleteById(Integer id){				
-		
-		 authorsRepository.deleteById(id);		
+
+	public void deleteById(Integer id) {
+
+		authorsRepository.deleteById(id);
 	}
 
 	private AuthorDto entityToDto(Author author) {
-	
-		AuthorDto dto= new AuthorDto();
+
+		AuthorDto dto = new AuthorDto();
 		dto.setId(author.getId());
 		dto.setSurname(author.getSurname());
 		dto.setName(author.getName());
-		
+
 		return dto;
 	}
-	
+
 	private Author dtoToEntity(AuthorDto dto) {
-		
-		Author author= new Author();
+
+		Author author = new Author();
 		author.setId(dto.getId());
 		author.setSurname(dto.getSurname());
 		author.setName(dto.getName());
-		
+
 		return author;
 	}
 }
