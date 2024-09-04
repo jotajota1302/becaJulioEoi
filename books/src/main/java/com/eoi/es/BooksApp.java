9e51ce8bfa04634a7d83a943bc3156974c71978b
jotajota1302 <jotajota1302@hotmail.com>
2024-09-04 16:30:12 +0200

    commit

--- books/src/main/java/com/eoi/es/BooksApp.java
+++ books/src/main/java/com/eoi/es/BooksApp.java
@@ -2,10 +2,12 @@ package com.eoi.es;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableEurekaClient 
 public class BooksApp {
 
 	public static void main(String[] args) {
